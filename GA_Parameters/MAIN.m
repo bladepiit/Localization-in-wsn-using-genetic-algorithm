2b402a4764ea2bab2a87b08cd084964ae83239d6
beroualimad <beroualimad5@gmail.com>
2023-05-26 22:41:46 +0100

    Update

--- GA_Parameters/MAIN.m
+++ GA_Parameters/MAIN.m
@@ -23,6 +23,7 @@ for SheetIndex = 1 : length(SheetNames)
     ListPopulation = [10,20,40,80,160]; % Define a list of population values
     beaconNodesPossition = Conf(1:NumBeaconNodes,:);
     UnkownNodesReelPosition = Conf(NumBeaconNodes+1:end,:);
+    
     for popIteration = 1 : length(ListPopulation)
         global PopSize;
         PopSize = ListPopulation(popIteration);
@@ -37,40 +38,91 @@ for SheetIndex = 1 : length(SheetNames)
             ErrorRateForOneIterationEGA = [];
             ErrorRateForOneIterationBGA = [];
 
+            % variable for EGA
             numBeaconNodes = NumBeaconNodes;
-            UnknownNodeIndex = numBeaconNodes + 1;
-            while UnknownNodeIndex <= numNodes
+            UnknownNodeIndex = 1;
+            beaconNodesPossitionEGA = beaconNodesPossition;
+            UnkownNodesReelPositionEGA = UnkownNodesReelPosition;
+
+            % variable for BGA
+            numBeaconNodesBGA = NumBeaconNodes;
+            UnknownNodeIndexBGA = 1;
+            beaconNodesPossitionBGA = beaconNodesPossition;
+            UnkownNodesReelPositionBGA = UnkownNodesReelPosition;
+
+            % EGA
+            while UnknownNodeIndex <= NumUnkownNodes
                 beaconTabOfUnkownNode = [];
                 unknownNodePosition = [];
-
+                % this is about how to calculate the distance between Unknown nodes and beacon node (EGA) 
                 for BeaconNodeIndex = 1 : numBeaconNodes
-                    distanceBeaconUnknown = Distance_Calculation(Conf(UnknownNodeIndex,:),Conf(BeaconNodeIndex,:));
+                    distanceBeaconUnknown = Distance_Calculation(UnkownNodesReelPositionEGA(UnknownNodeIndex,:),beaconNodesPossitionEGA(BeaconNodeIndex,:));
                     % when the unknown node is within range of the beacon node
                     if distanceBeaconUnknown < RSSI
-                        beaconTabOfUnkownNode(end+1,:) = [Conf(BeaconNodeIndex,1),Conf(BeaconNodeIndex,2)];
+                        beaconTabOfUnkownNode(end+1,:) = [beaconNodesPossitionEGA(BeaconNodeIndex,1),beaconNodesPossitionEGA(BeaconNodeIndex,2)];
                     end
                 end
+                
                 if size(beaconTabOfUnkownNode,1) >= 3
-                    unknownNodePosition = Conf(UnknownNodeIndex,:);
+                    unknownNodePosition = UnkownNodesReelPositionEGA(UnknownNodeIndex,:);
                     % Estimate Position for EGA
                     estimatedPositionEGA = findPosition(unknownNodePosition, beaconTabOfUnkownNode);
                     ErrorRateForOneIterationEGA = [ErrorRateForOneIterationEGA;estimatedPositionEGA(:,3)];
 
-                    % Estimate Position for BGA
-                    estimatedPositionBGA = findPosition_b(unknownNodePosition, beaconTabOfUnkownNode); 
-                    ErrorRateForOneIterationBGA = [ErrorRateForOneIterationBGA;estimatedPositionBGA(:,3)];
-
                     outputStr = sprintf('Unknown node position: %s\nBeacon in range: %s', mat2str(unknownNodePosition), mat2str(beaconTabOfUnkownNode));
                     disp(outputStr);
                     disp(estimatedPositionEGA);
-                    numBeaconNodes = numBeaconNodes + 1;
-                    UnknownNodeIndex = UnknownNodeIndex + 1;
+
+                    if estimatedPositionBGA(:,3) < 0.5
+                        beaconNodesPossitionEGA(end+1,:) = UnkownNodesReelPositionEGA(UnknownNodeIndex,:);
+                        numBeaconNodes = numBeaconNodes + 1;
+                    end
+                    
                 else
-                    ithRow = Conf(UnknownNodeIndex, :);
-                    Conf(UnknownNodeIndex, :) = [];
-                    Conf = [Conf; ithRow];
+                    ithRow = UnkownNodesReelPositionEGA(UnknownNodeIndex, :);
+                    UnkownNodesReelPositionEGA(UnknownNodeIndex, :) = [];
+                    UnkownNodesReelPositionEGA = [UnkownNodesReelPositionEGA; ithRow];
                 end
+                UnknownNodeIndex = UnknownNodeIndex + 1;
             end
+
+
+            % BGA
+           while UnknownNodeIndexBGA <= (numNodes - NumBeaconNodes)
+                beaconTabOfUnkownNodeBGA = [];
+                unknownNodePositionBGA = [];
+             
+                % this is about how to calculate the distance between Unknown nodes and beacon node (BGA) 
+                for BeaconNodeIndexBGA = 1 : numBeaconNodesBGA
+                    distanceBeaconUnknownBGA = Distance_Calculation(UnkownNodesReelPositionBGA(UnknownNodeIndexBGA,:),beaconNodesPossitionBGA(BeaconNodeIndexBGA,:));
+                    % when the unknown node is within range of the beacon node
+                    if distanceBeaconUnknownBGA < RSSI
+                        beaconTabOfUnkownNodeBGA(end+1,:) = [beaconNodesPossitionBGA(BeaconNodeIndexBGA,1),beaconNodesPossitionBGA(BeaconNodeIndexBGA,2)];
+                    end
+                end
+                if size(beaconTabOfUnkownNodeBGA,1) >= 3
+                    unknownNodePositionBGA = UnkownNodesReelPositionBGA(UnknownNodeIndexBGA,:);
+                   
+                    % Estimate Position for BGA
+                    estimatedPositionBGA = findPosition_b(unknownNodePositionBGA, beaconTabOfUnkownNodeBGA); 
+                    ErrorRateForOneIterationBGA = [ErrorRateForOneIterationBGA;estimatedPositionBGA(:,3)];
+
+                    outputStr = sprintf('Unknown node position: %s\nBeacon in range: %s', mat2str(unknownNodePositionBGA), mat2str(beaconTabOfUnkownNodeBGA));
+                    %disp(outputStr);
+                    %disp(estimatedPositionBGA);
+                    if estimatedPositionBGA(:,3) < 0.5
+                        beaconNodesPossitionBGA(end+1,:) = UnkownNodesReelPositionBGA(UnknownNodeIndexBGA,:);
+                        numBeaconNodesBGA = numBeaconNodesBGA + 1;
+                    end
+                    
+                else
+                    ithRowBGA = UnkownNodesReelPositionBGA(UnknownNodeIndexBGA, :);
+                    UnkownNodesReelPositionBGA(UnknownNodeIndexBGA, :) = [];
+                    UnkownNodesReelPositionBGA = [UnkownNodesReelPositionBGA; ithRowBGA];
+                end
+                UnknownNodeIndexBGA = UnknownNodeIndexBGA + 1;
+            end
+
             % Error Rate for EGA
             AverageErrorRateForOneIterationEGA = sum(ErrorRateForOneIterationEGA) / length(ErrorRateForOneIterationEGA);
             FileOfAverageErrorRateForAllEGA = [FileOfAverageErrorRateForAllEGA;AverageErrorRateForOneIterationEGA];
